cd05de026f48f475d9ad7338527ceb98918f6be5
hoganallsbrooks342-creator <hogan.allsbrooks342@students.cmcss.net>
2026-03-03 18:06:02 -0600

    hello

--- task2.py
+++ task2.py
@@ -7,34 +7,34 @@ from data import first, last, birth, days, p_first, p_last, phone, street, city,
 
 
 # first
-first = int()
+
 
 # last
-last = int()
+
 
 # birth
-birth = int()
+birth = int(birth)
 
 # days
-days = int()
+days = int(days)
 
 # p_first
-p_first = int()
+
 
 # p_last
-p_last = int()
+
 
 # phone
-phone = int()
+
 
 # street
-street = int()
+
 
 # city
-city = int()
+
 
 # state
-state = int()
+
 
 # zip_code
-zip_code = int()
+
